37e60e3acdfb8f38d2121440f88786c236f7f492
Sergey1-git <Sergey1-git.yan.dex.ru@yandex.ru>
2025-10-26 21:11:59 +0300

    create base.html, index.html, media, styles.css

--- flower_shop/flower/views.py
+++ flower_shop/flower/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+menu = [{'title': "Главная страница", 'url_name': 'home'},
+        {'title': "Контакты", 'url_name': 'contact'},
+]
+
+
+def index(request):
+    date={'title': 'Главная страница','menu': menu,'posts': 'db.sqlite3',}
+    return render(request, 'flower/index.html', context=date)
+
--- flower_shop/flower_shop/settings.py
+++ flower_shop/flower_shop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vay_oezw4g+kq3kdaul6nba@p3$-cc%g@w3eucj70rnhbz4)n0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1',]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'flower.apps.FlowerConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'flower_shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +121,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
