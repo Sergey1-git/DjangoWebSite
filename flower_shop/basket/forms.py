1aa6523ef25f0884ada33320bd19ee2a22351b3f
Sergey1-git <Sergey1-git.yan.dex.ru@yandex.ru>
2025-10-27 08:41:55 +0300

    create func basket, preparation_form into views.py

---
+++ flower_shop/basket/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+class FormBasket(forms.Form):
+
+
+    def __init__(self, dict_all, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Добавление полей из словаря
+        for name, field_class in dict_all.items():
+            if name !='csrfmiddlewaretoken':
+                print('name',name)
+                print('field_class', field_class)
+                self.fields[name] = field_class
+        self.post=None
--- flower_shop/basket/views.py
+++ flower_shop/basket/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from flower_shop.basket.forms import FormBasket
+from flower_shop.flower.models import Flower
+from django import forms
+
 
 
 dict_object_basket = {}
+dict_object_basket_form={}
 
 class Basket:
     def __init__(self, id):
@@ -10,9 +15,6 @@ class Basket:
 
 
     def add_flower_basket(id, *args):
-        #print(type(args[0]))
-        #print(type(args[1]))
-        print('Вход add_flower_basket ',id, *args)
         if len(args)==2:
             if args[0] not in dict_object_basket[id].args:
                 dict_object_basket[id].args[args[0]]=args[1]
@@ -22,6 +24,7 @@ class Basket:
 
     def delete_basket_and_form(id):
         del dict_object_basket[id]
+        del dict_object_basket_form[id]
 
 
 
@@ -37,3 +40,38 @@ def object_basket(id_user, *args):
         Basket.add_flower_basket(id_user, *args)
     else:
         Basket.add_flower_basket(id_user, *args)
+
+
+def basket(request):
+
+    if request.method == "GET" and len(dict_object_basket) > 0:
+        user_id = request.user.id
+        dict_data=dict_object_basket[user_id].args
+        dict_object_basket_form[user_id]=FormBasket(preparation_form(dict_data, user_id))
+
+        return render(request, 'basket/basket.html',{'title': 'Корзина покупок',
+                             'form': dict_object_basket_form[user_id],'args': len(dict_object_basket[user_id].args),})
+
+
+    if len(dict_object_basket)==0:
+        return render(request, 'basket/basket.html',{'title': 'Ваша корзина покупок пуста','args':0,})
+
+
+def preparation_form(dict_data, user_id):
+    dict_all = {}
+    dict_cost = {}
+    for key in dict_data:
+        initial = dict_object_basket[user_id].args[key]
+        w1 = Flower.objects.get(pk=key)
+        string = str(w1.id)
+        dict_all[string] = forms.IntegerField(min_value=0,
+                                              max_value=w1.quantity + initial, initial=initial, label=w1.title)
+        dict_cost[w1.id] = w1.price * dict_object_basket[user_id].args[key]
+    cost_order = 0
+    for key in dict_cost:
+        cost_order += dict_cost[key]
+    dict_all['cost'] = forms.IntegerField(label='Общая стоимость заказа', initial=cost_order,
+                                          widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    dict_all['address'] = forms.CharField(max_length=255, label='Адрес доставки')
+    dict_all['id_user'] = forms.CharField(widget=forms.HiddenInput(), initial=user_id)
+    return dict_all
